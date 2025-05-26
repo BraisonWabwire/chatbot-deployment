8c7f352066f863839b9511f0be11a67b5e859191
Braison Wabwire <braisonwabwire2003@gmail.com>
2025-05-26 15:31:06 +0300

    requirements.txt

--- chatbotproject/chatbotproject/settings.py
+++ chatbotproject/chatbotproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0qg&z+e76z_5p7e=$$cbk(3g!2s+h10cll#(@3of388j=gs9bg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
